d353790e72f017e36098352276e8ae9c7c306f19
Justin Rinehuls <jrinehuls@gmail.com>
2023-05-07 22:56:44 -0400

    Start adding equipment frmae

--- src/view/frames/EquipmentFrame.java
+++ src/view/frames/EquipmentFrame.java
@@ -6,41 +6,59 @@ import view.panels.PlayerPanel;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class EquipmentFrame extends JFrame {
+public class EquipmentFrame extends JFrame implements ActionListener {
 
     Player player;
 
-    JPanel currentWeaponPanel = new JPanel();
-    private static final int RADIO_PANEL_WIDTH = 300;
-    private static final int RADIO_PANEL_HEIGHT = 300;
-    private static final int RADIO_PANEL_COLS = 3;
+    JPanel currentPanel = new JPanel();
+    private static final int CURRENT_PANEL_WIDTH = 300;
+    private static final int CURRENT_PANEL_HEIGHT = 300;
+    private static final int CURRENT_PANEL_COLS = 1;
+
+    JPanel selectionPanel = new JPanel();
+    private static final int SELECTION_PANEL_WIDTH = 300;
+    private static final int SELECTION_PANEL_HEIGHT = 300;
+    private static final int SELECTION_PANEL_COLS = 3;
 
     JPanel buttonPanel = new JPanel();
-    private static final int BUTTON_PANEL_WIDTH = RADIO_PANEL_WIDTH;
+    private static final int BUTTON_PANEL_WIDTH = CURRENT_PANEL_WIDTH + SELECTION_PANEL_WIDTH;
     private static final int BUTTON_PANEL_HEIGHT = 50;
 
     JButton submitButton = new JButton("Submit");
     JButton cancelButton = new JButton("Cancel");
 
-    JPanel spellPanel;
+    JPanel equipmentPanel;
 
     public EquipmentFrame() {
 
         player = PlayerPanel.getPlayer();
 
         //--------------------Stuff for main panel---------------------------------------
-        spellPanel = new JPanel();
-        spellPanel.setLayout(new BoxLayout(spellPanel, BoxLayout.Y_AXIS));
-        spellPanel.setBorder(new EmptyBorder(new Insets(20, 20, 20, 20)));
+        equipmentPanel = new JPanel();
+        equipmentPanel.setLayout(new BoxLayout(equipmentPanel, BoxLayout.Y_AXIS));
+        equipmentPanel.setBorder(new EmptyBorder(new Insets(20, 20, 20, 20)));
+
+        //--------------------Stuff for left panel---------------------------------------
+
+        //--------------------Stuff for right panel---------------------------------------
+
+        //--------------------Stuff for bottom panel---------------------------------------
 
         //------------------------Frame stuff---------------------------------------------
-        this.add(spellPanel);
+        this.add(equipmentPanel);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.pack();
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         //this.setLocationRelativeTo(MainGameFrame.getFrames()[0]);
         this.setVisible(false);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
     }
 }
