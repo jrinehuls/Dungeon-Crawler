41606565742974b63a50f8f5bd4ec9a179612fc5
jrinehulsPGT <jrinehuls@pgttrucking.com>
2023-06-09 13:16:14 -0400

    Remove unnecessary comments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import view.frames.EquipmentFrame;
 import view.frames.MainGameFrame;
 
 public class Main {
 
-
 	public static void main(String[] args) {
 
-		// new EquipmentFrame();
 		new MainGameFrame();
 
 	}
