341a58420cfefff246417e71cb6c540a5d120291
jrinehulsPGT <jrinehuls@pgttrucking.com>
2023-10-13 16:45:22 -0400

    Make getters and setters for array and hashmaps

--- src/collections/shop/ShopCollection.java
+++ src/collections/shop/ShopCollection.java
@@ -37,7 +37,13 @@ public class ShopCollection {
 
     public ShopCollection() {
         this.setWeaponsMap();
-        this.setHeadGearsMapMap();
+        this.setHeadGearsMap();
+        this.setArmorMap();
+        this.setArmMap();
+        this.setFootWearsMap();
+        this.setAccessoriesMap();
+        this.setAttackItemsMap();
+        this.setHealingItemsMap();
     }
 
     // ----------------------------- Weapon Stuff -----------------------------
@@ -49,7 +55,7 @@ public class ShopCollection {
         return this.weaponsMap;
     }
 
-    public void setWeaponsMap() {
+    private void setWeaponsMap() {
         for (Weapon weapon: this.weaponsArray) {
             this.weaponsMap.put(this.generateKey(weapon), weapon);
         }
@@ -64,12 +70,103 @@ public class ShopCollection {
         return this.headGearsMap;
     }
 
-    public void setHeadGearsMapMap() {
+    private void setHeadGearsMap() {
         for (Head headGear: this.headGearsArray) {
             this.headGearsMap.put(this.generateKey(headGear), headGear);
         }
     }
 
+    // ---------------------------- Armor Gear Stuff ----------------------------
+    public Body[] getArmorArray() {
+        return this.armorArray;
+    }
+
+    public HashMap<String, Body> getArmorMap() {
+        return this.armorMap;
+    }
+
+    private void setArmorMap() {
+        for (Body armor: this.armorArray) {
+            this.armorMap.put(this.generateKey(armor), armor);
+        }
+    }
+
+    // ----------------------------- Arm Gear Stuff -----------------------------
+    public Arm[] getArmArray() {
+        return this.armArray;
+    }
+
+    public HashMap<String, Arm> getArmMap() {
+        return this.armMap;
+    }
+
+    private void setArmMap() {
+        for (Arm arm: this.armArray) {
+            this.armMap.put(this.generateKey(arm), arm);
+        }
+    }
+
+    // ----------------------------- Feet Stuff -----------------------------
+    public Feet[] getFootWearsArray() {
+        return this.footWearsArray;
+    }
+
+    public HashMap<String, Feet> getFootWearsMap() {
+        return this.footWearsMap;
+    }
+
+    private void setFootWearsMap() {
+        for (Feet foot: this.footWearsArray) {
+            this.footWearsMap.put(this.generateKey(foot), foot);
+        }
+    }
+
+    // ----------------------------- Accessory Stuff -----------------------------
+    public Accessory[] getAccessoriesArray() {
+        return this.accessoriesArray;
+    }
+
+    public HashMap<String, Accessory> getAccessoriesMap() {
+        return this.accessoriesMap;
+    }
+
+    private void setAccessoriesMap() {
+        for (Accessory accessory: this.accessoriesArray) {
+            this.accessoriesMap.put(this.generateKey(accessory), accessory);
+        }
+    }
+
+    // ----------------------------- Attack Items Stuff -----------------------------
+    public AttackItem[] getAttackItemsArray() {
+        return this.attackItemsArray;
+    }
+
+    public HashMap<String, AttackItem> getAttackItemsMap() {
+        return this.attackItemsMap;
+    }
+
+    private void setAttackItemsMap() {
+        for (AttackItem attackItem: this.attackItemsArray) {
+            this.attackItemsMap.put(this.generateKey(attackItem), attackItem);
+        }
+    }
+
+    // ----------------------------- Healing Items Stuff -----------------------------
+    public HealingItem[] getHealingItemsArray() {
+        return this.healingItemsArray;
+    }
+
+    public HashMap<String, HealingItem> getHealingItemsMap() {
+        return this.healingItemsMap;
+    }
+
+    private void setHealingItemsMap() {
+        for (HealingItem healingItem: this.healingItemsArray) {
+            this.healingItemsMap.put(this.generateKey(healingItem), healingItem);
+        }
+    }
+
+    // -------------------------------- Key Stuff --------------------------------
     public String generateKey(Item item) {
         return item.getName() + " - Price: " + item.getCost();
     }
